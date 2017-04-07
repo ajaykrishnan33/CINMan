4d073465c7b7c7ad24af538084e1639eb8ff67b4
AbzP <patraabinash55@gmail.com>
2017-04-07 10:46:33 +0530

    post request | usb detect

---
+++ daemon/usb_detect.py
@@ -0,0 +1,35 @@
+import subprocess
+import requests
+import netifaces as ni
+import datetime
+import json
+import os
+import sys
+import time
+
+machineid = 2
+ipaddr = ni.ifaddresses("wlan0")[2][0]['addr']
+
+headers = {"Authorization" : "Token 5f05236f9e0186437e6f8e9e9217e32072f31952"}
+
+SERVER_URL = "http://localhost:5000/app/logentry/"
+disks = 'lsblk -PS | grep "disk" | grep '
+usb = "'"+'TRAN="usb"'+"'" + "> usb_detect" 
+delay=5 #in seconds
+data = {}
+
+while True:
+	disks = 'lsblk -PS | grep "disk" | grep '
+	usb = "'"+'TRAN="usb"'+"'" + "> usb_detect" 
+	a=os.system(disks+usb)
+	if a== 0:
+		with open('usb_detect','r') as f:
+			d= f.read().replace('\n','')
+		vendor = d.split('VENDOR="')[1].split('"')[0]
+		model = d.split('MODEL="')[1].split('"')[0]
+		data= {"event" : "external usb","vendor" : vendor,"model":model}
+		f.close()
+		notif=json.dumps(data)
+		r = requests.post(SERVER_URL, data={"log_entry_type":2, "text":notif, "machine":machineid}, headers=headers)
+	time.sleep(delay)
+	
