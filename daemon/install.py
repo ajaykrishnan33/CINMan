ebd16ae006a9485f3c05579d75ed5a11206cca5c
ajaykrishnan33 <akj1996@gmail.com>
2017-04-17 01:15:42 +0530

    server ip input

--- daemon/install.py
+++ daemon/install.py
@@ -1,10 +1,11 @@
 import requests
 
+server_ip = raw_input("Enter server ip:")
 username = raw_input("Enter username:")
 password = raw_input("Enter password:")
 
-SERVER_URL = "http://localhost:8000/app/"
-LOGIN_URL = "http://localhost:8000/api-token-auth/"
+SERVER_URL = "http://" + server_ip + "/app/"
+LOGIN_URL = "http://" + server_ip + "/api-token-auth/"
 
 r = requests.post(LOGIN_URL, data={"username":username, "password":password})
 token = r.json()["token"]
